a25d41ad74b1c528ba9b4d33250d850b4296623b
Allen <allendinghamilton@gmail.com>
2016-05-14 00:55:45 -0400

    broken

--- teampointr-android/app/src/main/java/teampointr/teampointr/FacebookLoginActivity.java
+++ teampointr-android/app/src/main/java/teampointr/teampointr/FacebookLoginActivity.java
@@ -1,5 +1,9 @@
 package teampointr.teampointr;
 
+
+import android.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,10 +13,21 @@ import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
-public class FacebookLoginActivity extends AppCompatActivity {
+public static class FacebookLoginActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facebook_login);
+
+        if (savedInstanceState != null) {
+            return;
+        }
+
+        // Create a new Fragment to be placed in the activity layout
+        FacebookLoginFragment facebookLoginFragment = new FacebookLoginFragment();
+
+        // Add the fragment to the 'fragment_container' FrameLayout
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, facebookLoginFragment).commit();
     }
 }
